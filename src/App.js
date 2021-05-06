7865b1f9911667241db7fe4a367e12308e037230
dilumharshana <dilum.harshana123@gmail.com>
2021-05-06 19:27:13 +0530

    react app fundamentals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function na(){
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <h1>Started React</h1>
+  )
 }
 
-export default App;
+export default na;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
+
+
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+
+const Fn = props => {
+
+  const element = (
+    <div>
+      <h1>Hello , {props.name}</h1>
+      <p>Your age is {props.age}</p>
+    </div> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    )
+}
+
+const Tm = () => {
+
+      const Ti = <p>{new Date().toLocaleTimeString()}</p>
+    
+      ReactDOM.render(
+        Ti,
+        document.getElementById('time')
+      )
+    }
+
+Fn("Dlum Harshana" , 23)   
+setInterval(Tm ,1000);
+
+
+
+
+
+
 reportWebVitals();
