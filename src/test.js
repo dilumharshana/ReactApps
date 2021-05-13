2c1353a80018e6e73cff46bee594d4a87b83f945
dilumharshana <dilum.harshana123@gmail.com>
2021-05-13 22:13:54 +0530

    controlled componenet revesion

--- src/test.js
+++ src/test.js
@@ -1,36 +1,43 @@
 import React, { Component, Fragment } from "react";
-import Item from './test2'
-
+import Item from "./test2";
 
 class Form extends React.Component{
 
     constructor(props){
-        super(props);
 
-        this.state = {value:''}
+      super(props){
 
+        this.state = {value:''};
+
+        this.submit = this.submit.bind(this);
+        this.changeValue = this.changeValue.bind(this);
+      }
     }
-    
-    render(){
 
-        this.render(
+    changeValue(event){
 
-            <div>
-                <form onSubmit={this.submit}>
-                <select name="phone" id="">
-
-                    <option value={this.props.value} name="s20">Samsung s20</option>
-                    <option value={this.props.value} name="11max" >Iphone 11</option>
-
-                    </select>
-                                    </form>
-            </div>
-
-        )
+      this.setState( {value:event.target.value});
     }
+
+    submit(){
+      alert(`Hello ${this.state.value} , please chek your gmail inbox to confirm your email !`);
+    }
+
+
+  render(){
+    return(
+
+        <form action="" onSubmit={this.submit}>
+
+          <label htmlFor="">
+            User Name:
+          </label>
+
+          <input type="text" value={this.state.value} onChange={this.changeValue} />
+
+        </form>
+    )
+  }
+
 }
-
-
- 
-
 export default Form;
